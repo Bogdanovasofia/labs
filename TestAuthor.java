b2f8756fca5faf95b00a7c3274ecdd1bb3748640
Bogdanovasofia <56391388+Bogdanovasofia@users.noreply.github.com>
2019-12-05 14:53:09 +0300

    Add files via upload

---
+++ TestAuthor.java
@@ -0,0 +1,8 @@
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author a1 = new Author("Софья", "bgdnvsn@yandex.ru",'F');
+        System.out.println(a1);
+        a1.setEmail("bgdnvsn@yandex.ru");
+        System.out.println(a1);
+    }
+}
---
+++ TestBall1.java
@@ -0,0 +1,8 @@
+public class TestBall1 {
+    public static void main(String[] args) {
+        Ball1 b1 = new Ball1(150, 150);
+        System.out.println(b1);
+        b1.move(35, 25);
+        System.out.println(b1);
+    }
+}
